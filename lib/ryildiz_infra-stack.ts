6108443df612e43e0211b0cd581e12a907c187e6
rozaydin <ridvanozaydin@gmail.com>
2024-10-05 21:02:34 +0300

    updates
    Signed-off-by: rozaydin <ridvanozaydin@gmail.com>

--- lib/ryildiz_infra-stack.ts
+++ lib/ryildiz_infra-stack.ts
@@ -5,72 +5,267 @@ import * as route53 from "aws-cdk-lib/aws-route53";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as ecrdeploy from "cdk-ecr-deployment";
+
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
-import { ApplicationProtocol } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import {
+  ApplicationProtocol,
+  ApplicationProtocolVersion,
+  Protocol,
+} from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import { LoadBalancerTarget } from "aws-cdk-lib/aws-route53-targets";
+import { Duration } from "aws-cdk-lib";
+import { DockerImageAsset } from "aws-cdk-lib/aws-ecr-assets";
+import * as path from "node:path";
+import { LoadBalancingProtocol } from "aws-cdk-lib/aws-elasticloadbalancing";
 
 export class RyildizInfraStack extends cdk.Stack {
+  private PREFIX = "krs";
+  private DOMAIN_NAME = "test.ridvanozaydin.com";
+  private ZONE_NAME = "ridvanozaydin.com";
+  private SERVICE_NAME = "krs-service";
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const ecsClusterName = "rozaydin-test-cluster";
-    const domainName = "test.ridvanozaydin.com";
-    const zoneName = "ridvanozaydin.com";
-    const serviceName = "rozaydin-test-service";
 
     // use existing default vpc
     const vpc = ec2.Vpc.fromLookup(this, "default", {
       isDefault: true,
     });
 
-    const domainZone = route53.HostedZone.fromLookup(this, "HostedZone", {
-      domainName: zoneName,
+    // Create ECR registry
+    const containerRegistry = new ecr.Repository(this, "ContainerRegistry", {
+      emptyOnDelete: true,
+      repositoryName: `${this.PREFIX}_registry`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      lifecycleRules: [
+        {
+          description: "Keeps a maximum number of images to minimize storage",
+          maxImageCount: 10,
+        },
+      ],
     });
 
-    // create a new certificate
+    // build and push initial image into ECR registry
+
+    const localContainerImage = new DockerImageAsset(this, "DockerImageAsset", {
+      directory: path.join(__dirname, "../../krs-loyalty-be-fargate/"),
+      buildArgs: {
+        platform: "linux/amd64",
+      },
+      invalidation: {
+        buildArgs: false,
+      },
+    });
+
+    const dockerImageAssetHash = localContainerImage.assetHash;
+    const destinationImageName = `${containerRegistry.repositoryUri}:${dockerImageAssetHash}`;
+    const destinationImageNameLatest = `${containerRegistry.repositoryUri}:latest`;
+
+    /* This is a workaround, since cdk does not provide a way to use custom ECR */
+    // https://github.com/cdklabs/cdk-ecr-deployment
+
+    const ecrDeployment = new ecrdeploy.ECRDeployment(
+      this,
+      "EcrDeployment:withHash",
+      {
+        src: new ecrdeploy.DockerImageName(localContainerImage.imageUri),
+        dest: new ecrdeploy.DockerImageName(destinationImageName),
+      }
+    );
+
+    const ecrDeploymentlatest = new ecrdeploy.ECRDeployment(
+      this,
+      "EcrDeployment:latest",
+      {
+        src: new ecrdeploy.DockerImageName(localContainerImage.imageUri),
+        dest: new ecrdeploy.DockerImageName(destinationImageNameLatest),
+      }
+    );
+
+    const domainZone = route53.HostedZone.fromLookup(this, "HostedZone", {
+      domainName: this.ZONE_NAME,
+    });
+
     const certificate = new acm.Certificate(this, "Certificate", {
-      domainName,
-      certificateName: domainName, // Optionally provide an certificate name
+      domainName: this.DOMAIN_NAME,
+      certificateName: this.DOMAIN_NAME,
       validation: acm.CertificateValidation.fromDns(domainZone),
     });
 
     const cluster = new ecs.Cluster(this, "Cluster", {
       vpc: vpc,
-      clusterName: "rozaydin-test-cluster",
+      clusterName: `${this.PREFIX}_cluster`,
+    });
+
+    /**
+     * Fargate services use the default VPC Security Group unless
+     * one or more are provided using the securityGroups property
+     * in the constructor.
+     */
+
+    /**
+     * ApplicationMultipleTargetGroupsFargateService
+     * if more than one application target group are needed,
+     * instantiate one of the following:
+     */
+
+    const taskDefinition = new ecs.TaskDefinition(this, "ECSTaskDefinition", {
+      compatibility: ecs.Compatibility.FARGATE,
+      cpu: "512",
+      memoryMiB: "2048",
+    });
+
+    const container = taskDefinition.addContainer("Container", {
+      image: ecs.ContainerImage.fromEcrRepository(containerRegistry),
+      portMappings: [
+        {
+          // for socket.io
+          containerPort: 8433,
+          protocol: ecs.Protocol.TCP,
+        },
+        {
+          // for health check
+          containerPort: 8080,
+          protocol: ecs.Protocol.TCP,
+        },
+      ],
+      healthCheck: {
+        // health check for the task
+        command: ["CMD-SHELL", "curl -f http://localhost:8080 || exit 1"],
+        // the properties below are optional
+        interval: Duration.minutes(1),
+        retries: 3,
+        startPeriod: Duration.minutes(1),
+        timeout: Duration.minutes(1),
+      },
+      memoryLimitMiB: 2048,
+      environment: {
+        WEB_SOCKET_PORT: "8443",
+        EXPRESS_PORT: "8080",
+        USE_TEST_CARD_NUMBER: "true",
+        TEST_CARD_NUMBER: "7020113200035014",
+        RECEIPT_PRINTER_CHARACTER_PER_LINE: "48",
+        RECEIPT_PRINTER_CHARACTER_ENCODING: "multilingual",
+        // FOLLOWINGS SECRETS ARE AVAILABLE IN TERMINAL ALSO
+        WEB_SOCKET_COMMUNICATION_SECRET: "eb8600ca-1122-43e5-8506-1fededbf5aee",
+        WEB_SOCKET_INQUIRY_CHANNEL: "web.socket.inquiry.channel",
+        WEB_SOCKET_RECEIPT_CHANNEL: "web.socket.receipt.channel",
+      },
+      containerName: `${this.PREFIX}_container`,
     });
 
     const appLoadBalancerService =
       new ecs_patterns.ApplicationLoadBalancedFargateService(
         this,
-        ecsClusterName,
+        "ApplicationLoadBalancedFargateService",
         {
-          cluster: cluster, // Required
-          cpu: 512, // Default is 256
-          desiredCount: 1, // Default is 1,
+          cluster: cluster,
+          cpu: 512,
+          desiredCount: 1,
           assignPublicIp: true,
+          listenerPort: 8433,
+          taskDefinition,
+          /*
           taskImageOptions: {
-            // image: ecs.ContainerImage.fromAsset('./image') // build and upload an image directly from a Dockerfile in your source directory.
-            image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+            image: ecs.ContainerImage.fromEcrRepository(containerRegistry),
             environment: {
-              // TEST_ENVIRONMENT_VARIABLE1: "test environment variable 1 value",
-              // TEST_ENVIRONMENT_VARIABLE2: "test environment variable 2 value",
+              WEB_SOCKET_PORT: "8443",
+              EXPRESS_PORT: "8080",
+              USE_TEST_CARD_NUMBER: "true",
+              TEST_CARD_NUMBER: "7020113200035014",
+              RECEIPT_PRINTER_CHARACTER_PER_LINE: "48",
+              RECEIPT_PRINTER_CHARACTER_ENCODING: "multilingual",
+              // FOLLOWINGS SECRETS ARE AVAILABLE IN TERMINAL ALSO
+              WEB_SOCKET_COMMUNICATION_SECRET:
+                "eb8600ca-1122-43e5-8506-1fededbf5aee",
+              WEB_SOCKET_INQUIRY_CHANNEL: "web.socket.inquiry.channel",
+              WEB_SOCKET_RECEIPT_CHANNEL: "web.socket.receipt.channel",
             },
+            containerPort: 8443,
           },
-          protocol: ApplicationProtocol.HTTPS,
+          */
+          // protocol: ApplicationProtocol.HTTPS,
+          // targetProtocol: ApplicationProtocol.HTTP,
           redirectHTTP: true,
           certificate,
-          serviceName,
-          memoryLimitMiB: 2048, // Default is 512
-          publicLoadBalancer: true, // Default is true
+          serviceName: this.SERVICE_NAME,
+          // securityGroups: 
+          memoryLimitMiB: 2048,
+          publicLoadBalancer: true,
         }
       );
 
-    new route53.ARecord(this, "HttpsFargateAlbARecord", {
+    // health check for target group
+    appLoadBalancerService.targetGroup.configureHealthCheck({
+      path: "/",
+      port: "8080",
+      protocol: Protocol.HTTP,
+      interval: Duration.seconds(30),
+      healthyHttpCodes: "200",
+      healthyThresholdCount: 2,
+      unhealthyThresholdCount: 2,
+      timeout: Duration.seconds(4),
+    });
+
+    const aRecord = new route53.ARecord(this, "HttpsFargateAlbARecord", {
       zone: domainZone,
-      recordName: domainName,
+      recordName: this.DOMAIN_NAME,
       target: route53.RecordTarget.fromAlias(
         new LoadBalancerTarget(appLoadBalancerService.loadBalancer)
       ),
     });
+
+    // create cicd user, and role it will assume
+
+    const cicdUser = new iam.User(this, "iamUser", {
+      userName: `${this.PREFIX}_cicd_user`,
+    });
+
+    const ecrPermission = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          actions: ["ecr:*"],
+          resources: [containerRegistry.repositoryArn],
+        }),
+      ],
+    });
+
+    /*
+    const ecsPermission = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          actions: ["ecs:*"],
+          resources: [cluster.clusterArn],
+        }),
+      ],
+    });
+*/
+
+    const cicdRole = new iam.Role(this, "iamRole", {
+      assumedBy: new iam.ArnPrincipal(cicdUser.userArn),
+      roleName: `${this.PREFIX}_cicd_role`,
+      description: "CICD Role",
+      inlinePolicies: {
+        ECR_Permissins: ecrPermission,
+      },
+    });
+
+    const stsPermissions = new iam.PolicyDocument({
+      statements: [
+        new iam.PolicyStatement({
+          actions: ["sts:AssumeRole", "sts:TagSession"],
+          resources: [cicdRole.roleArn],
+        }),
+      ],
+    });
+
+    const inlinePolicy = new iam.Policy(this, "cicdUserInlinePolicy", {
+      document: stsPermissions,
+    });
+
+    cicdUser.attachInlinePolicy(inlinePolicy);
   }
 }
